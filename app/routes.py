109271bb1649a9644e1ddb198f2babb46aa06ba6
markru <this.markru@gmail.com>
2020-04-25 23:19:37 -0700

    added initial html

--- app/routes.py
+++ app/routes.py
@@ -4,4 +4,19 @@ from app import app
 @app.route("/")
 @app.route("/index")
 def index():
-    return "Hello World!"
+    user = {"username": "MarkyMark"}
+    return (
+        """
+    <html>
+        <head>
+            <title>Home Page - Microblog</title>
+        </head>
+        <body>
+            <h1>Hello, """
+        + user["username"]
+        + """!</h1>
+        </body>
+
+    </html>"""
+    )
+
